469273eacc6d9a0989abbd425b8da2e0f1fe4d32
YUNAHRAM <woojookong@naver.com>
2023-02-15 21:58:13 +0900

    no message

--- dosiraktong/js/script.js
+++ dosiraktong/js/script.js
@@ -20,6 +20,8 @@ window.onload = function () {
 
   // 4.모바일 버튼을 클릭을 하면 모바일 메뉴에 active 클래스를 추가한다.
   mbBt.addEventListener('click', function () {
+    // html scroll 없애기
+    htmlTag.classList.toggle('active');
     // 로고에 active 클래스를 추가하낟.
     logo.classList.toggle('active-blue');
     // 모바일 버튼에 active 클래스를 추가한다.
@@ -27,7 +29,7 @@ window.onload = function () {
     // 모바일 메뉴에 active 클래스를추가한다.
     mbNav.classList.toggle('active');
     // 모바일 버튼 아이콘 색상 검은색
-    htmlTag.classList.toggle('active');
+
     mbBtSpan.forEach(item => {
       item.classList.add('active');
     });
@@ -55,9 +57,8 @@ window.onload = function () {
         });
       } else {
         mbBtSpan.forEach(item => {
-          item.classList.toggle('active');
+          item.classList.remove('active');
         });
-        //아람 toggle 인지 remove 인지 확인하기
       }
       logo.classList.remove('active-blue');
     }
@@ -105,5 +106,124 @@ window.onload = function () {
     });
   }
 
-  new Swiper('.swvisual');
+  //  data.json 외부연동
+  // 1. XMLHttpRequest 활용 : 반드시 JSON.parse(); 실행하기
+  // xhttp.send(); 주석처리해놓음
+  const xhttp = new XMLHttpRequest();
+  //  data.json 이 불러들여졌는지 검사후 완료시 실행
+  xhttp.onreadystatechange = function (e) {
+    const req = e.target;
+    if (req.readyState === XMLHttpRequest.DONE) {
+      //   console.log('여그 자료가 왔어요.');
+      //   console.log(req.response);
+      //   아래 구문을 반드시 체크하자.
+      const dataArr = JSON.parse(req.response);
+      //   console.log(dataArr);
+    }
+  };
+  xhttp.open('GET', 'data.json');
+  //   xhttp.send();
+
+  //2.fetch  활용 : 아래 구문을 준수하자.
+  fetch('data.json')
+    .then(res => res.json())
+    .then(data => {
+      // 데이터를 활용한다.
+      // console.log(data);
+      //   const dataArr = data.visual;
+      // 데이터를 외부 변수에 저장한다.
+      visualData = data.visual;
+
+      //데이터 로딩후 데이터 개수만큼 li 태그를 만든다.
+      //만들어진 글자를 모아서 swUl 태그 안쪽에 innerHtml 한다.
+      let html = '';
+      let count = 1;
+      visualData.forEach(item => {
+        html += `<li>${count++}</li>`;
+      });
+      swUl.innerHTML = html;
+      //   자바스크립트가  li를 참조하도록 적용
+      swList = document.querySelectorAll('.swvisual-list li');
+      //li 태그를 클릭해서 슬라이드 이동하기
+      swListShow();
+
+      showVT(visualData[0], 0);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+
+  // 비주얼에 활용할 데이터
+  let visualData;
+  // 화면에 데이터를 출력하는 함수
+  const swTitle = document.querySelector('.swvisual-title');
+  const swTxt = document.querySelector('.swvisual-txt');
+  const swLink = document.querySelector('.swvisual-link');
+  //   const swList = document.querySelectorAll('.swvisual-list li');
+  const swUl = document.querySelector('.swvisual-list');
+  //   li 는 동적(데이터로딩후 )으로 만들어야 한다.
+  // const 는 내용을 변경하지 못한다.
+  let swList;
+
+  // 타이틀 내용 보여주기
+  function showVT(_data, _idx) {
+    console.log(_data);
+    swTitle.innerHTML = _data.title;
+    swTxt.innerHTML = _data.txt;
+    if (_data.link === 'no') {
+      swLink.classList.add('active');
+    } else {
+      swLink.classList.remove('active');
+    }
+    // 라인 모션 실행
+    changeBar(_idx);
+  }
+
+  //  포커스 라인 애니메이션 실행함수
+  function changeBar(_idx) {
+    // item: 각각의 li 를 의미
+    // index : 순서번호
+    swList.forEach((item, index) => {
+      if (index === _idx) {
+        item.classList.add('active');
+      } else {
+        item.classList.remove('active');
+      }
+    });
+  }
+
+  // li 를 클릭시 슬라이드 변경함수
+  function swListShow() {
+    swList.forEach((item, index) => {
+      // 클릭시 슬라이드 변경
+      item.addEventListener('click', function () {
+        // swVisual 슬라이드를 변경 (API 참조)
+        // swVisual.slideTo()
+        // swVisual.slideToLoop(번호, 속도, 효과)
+        swVisual.slideToLoop(index, 500, false);
+      });
+    });
+  }
+
+  // 비주얼 슬라이드
+  let swVisual = new Swiper('.swvisual', {
+    effect: 'fade',
+    loop: true,
+    speed: 1000,
+    autoplay: {
+      delay: 1000,
+      disableOnInteraction: false,
+    },
+    navigation: {
+      prevEl: '.swvisual-prev',
+      nextEl: '.swvisual-next',
+    },
+  });
+  // 슬라이드가 변경될 때마다 하고 싶은일 진행
+  swVisual.on('slideChange', function () {
+    console.log('진짜 html 태그의 순서', swVisual.realIndex);
+    console.log('모션이 되는 순서', swVisual.activeIndex);
+    // 텍스트를 수정한다.
+    showVT(visualData[swVisual.realIndex], swVisual.realIndex);
+  });
 };
